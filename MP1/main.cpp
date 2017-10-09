549762b7d42b500a218303b1f1b7eb585c174439
s123unny <s123unny@gmail.com>
2017-10-09 21:44:58 +0800

    [MP1]Fix bugs

--- MP1/main.cpp
+++ MP1/main.cpp
@@ -5,20 +5,10 @@
 #include "list_file.h"
 #include <openssl/md5.h>
 #include <unordered_map>
-#include <vector>
 #include <string>
-
-struct Set {
-	char c_md5[33], c_filename[256];
-};
 
 bool myfunction (const char* i, const char* j) { return strcmp(i, j) < 0; }
 
-void free_pointers(char *pointers[], int length) {
-	for (int i = 0; i < length; i++) {
-		free(pointers[i]);
-	}
-}
 void get_file_path(char dest[256], const char dir[],const char file[]) {
 	strcpy(dest, dir);
 	char *temp = dest + strlen(dest) + 1;
@@ -27,11 +17,10 @@ void get_file_path(char dest[256], const char dir[],const char file[]) {
 }
 
 void MD5_generator(const char file[256], char md5string[33], const char path[]) {
-	char command[300] = "md5sum ", postfix[] = " | cut -d ' ' -f 1";
+	char command[300] = "md5sum ";
 	char file_path[256];
 	get_file_path(file_path, path, file);
 	strcat(command, file_path);
-	strcat(command, postfix);
 	FILE *fp = popen(command, "r");
 	fread(md5string, sizeof(char)*32, 1, fp);
 	md5string[32] = '\0';
@@ -49,10 +38,15 @@ void status_commit(char const from_argv2[], int type, char record_path[]) {
 			record_fp = stdout;
 		} else {
 			//create file
+			if (file_names.length == 2) {
+				free_file_names(file_names);
+				return;
+			}
 			record_fp = fopen(record_path, "w");
+			fprintf(record_fp, "# commit 1\n");
 		}
 		//write
-		fprintf(record_fp, "# commit 1\n[new_file]\n");
+		fprintf(record_fp, "[new_file]\n");
 		for (int i = 2; i < file_names.length; i++) {
 			fprintf(record_fp, "%s\n", file_names.names[i]);
 		}
@@ -69,18 +63,18 @@ void status_commit(char const from_argv2[], int type, char record_path[]) {
 		free_file_names(file_names);
 	} else {
 		fseek(record_fp, -1, SEEK_END);
-		int new_file[1000], modified[1000], copied[1000], nf = 0, mo = 0, co = 0;
-		char *copyfrom[1000], buffer[550000];
+		int new_file[file_names.length], modified[file_names.length], copied[file_names.length], nf = 0, mo = 0, co = 0;
+		char copyfrom[file_names.length][256];
 		//remember delete . and ..
 		
 		char all_md5[file_names.length][33]; //for commit
 		std::unordered_map<std::string, std::string> line_in_map;
-		std::vector<Set> line;
+		char line_filename[file_names.length][256], line_md5[file_names.length][33];
 		std::string md5, filename;
-		Set temp;
 
 		long pos =  ftell(record_fp);
 	    pos = std::min(pos, (long)550000);
+	    char buffer[pos+1];
 	    fseek(record_fp, -pos, SEEK_CUR);
 	    fread(buffer, sizeof(char), pos, record_fp);
 	    buffer[pos] = '\0';
@@ -88,56 +82,51 @@ void status_commit(char const from_argv2[], int type, char record_path[]) {
 	    char *start = strrchr(buffer, ')');
 	    start += 2;
 
-	    int offset;
-		while (sscanf(start, "%s%s%n", temp.c_filename, temp.c_md5, &offset) == 2) {
+	    int offset, line_index = 0;
+		while (sscanf(start, "%s%s%n", line_filename[line_index], line_md5[line_index], &offset) == 2) {
 			start += offset;
-			filename = temp.c_filename;
-			md5 = temp.c_md5;
+			filename = line_filename[line_index];
+			md5 = line_md5[line_index];
 			//printf("%s==%s\n", temp.c_filename, temp.c_md5);
-			line.push_back(temp);
 			line_in_map.emplace(md5, filename);
+			line_index ++;
 		}
-
-		int line_index = 0;
-
+		line_index = 0;
 		for (int i = 2; i < file_names.length; i++) {
-			//match
-			if (strcmp(line[line_index].c_filename, file_names.names[i]) == 0) {
-				//exist -> check if modified
+			if (strcmp(file_names.names[i], ".loser_record") != 0) {
 				char new_md5[33];
 				MD5_generator(file_names.names[i], new_md5, from_argv2);
-
-				if (strcmp(new_md5, line[line_index].c_md5) != 0) {
-					modified[mo] = i;
-					mo ++;
-				}
-				if (!type) {
-					strcpy(all_md5[i], new_md5);
-				}
-				line_index ++;
-			} else {
-				//new or copy
-				if (strcmp(file_names.names[i], ".loser_record") != 0) {
-					char new_md5[33];
-					MD5_generator(file_names.names[i], new_md5, from_argv2);
+				//match
+				if (strcmp(line_filename[line_index], file_names.names[i]) == 0) {
+					//exist -> check if modified
+					if (strcmp(new_md5, line_md5[line_index]) != 0) {
+						modified[mo] = i;
+						mo ++;
+					}
+					line_index ++;
+				} else {
+					//new or copy
 					std::unordered_map<std::string,std::string>::iterator found = line_in_map.find(new_md5);
 					if (found == line_in_map.end()) {
 						new_file[nf] = i;
 						nf ++;
 					} else {
 						copied[co] = i;
-						copyfrom[co] = (char *)malloc(sizeof(char) * 256);
 						strcpy(copyfrom[co], found->second.c_str());
 						co ++;
 					}
-					if (!type) {
-						strcpy(all_md5[i], new_md5);
-					}
+				}
+				if (!type) {
+					strcpy(all_md5[i], new_md5);
 				}
 			}
 		}
 		if (!type) {
 			//find which commit
+			if (nf == 0 && mo == 0 && co == 0) {
+				free_file_names(file_names);
+				return;
+			}
 			start = strrchr(buffer, '#');
 			start = strchr(start+2, ' ');
 			start ++;
@@ -154,6 +143,9 @@ void status_commit(char const from_argv2[], int type, char record_path[]) {
 		if (type) {
 			record_fp = stdout;
 		}
+		char buf[6000];
+		setvbuf(record_fp, buf, _IOFBF, sizeof(buf));
+
 		fprintf(record_fp, "[new_file]\n");
 		for (int i = 0; i < nf; i++) {
 			fprintf(record_fp, "%s\n", file_names.names[new_file[i]]);
@@ -174,7 +166,6 @@ void status_commit(char const from_argv2[], int type, char record_path[]) {
 				}
 			}
 		}
-		free_pointers(copyfrom, co);
 		free_file_names(file_names);
 	}
 }
@@ -202,7 +193,7 @@ int main(int argc, char const *argv[])
 		type = argv[1][0];
 	} else {
 		char config_path[256], config[] = ".loser_config";
-		get_file_path(config_path, argv[3], config);
+		get_file_path(config_path, argv[argc-1], config);
 		FILE *fp = fopen(config_path, "r");
 		//if (fp == NULL) printf("QQQ\n");
 		type = feature_of_config(fp, argv[1]);
@@ -225,7 +216,7 @@ int main(int argc, char const *argv[])
 		}
 		FILE *record_fp = fopen(record_path, "r");
 		if (record_fp != NULL) {
-			char buffer[550000];
+			char buffer[550001];
 			fseek(record_fp, 0, SEEK_END);
 			while (time--) {
 				long pos =  ftell(record_fp);
